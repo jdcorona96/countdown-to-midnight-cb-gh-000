6ab8bc18c446ecae7d2cad341a8901962fd70649
Joseph Corona <josed.hc96@gmail.com>
2018-11-13 23:55:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,12 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep(1)
+def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 end
